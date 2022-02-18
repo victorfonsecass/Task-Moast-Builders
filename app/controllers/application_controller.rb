654d1230574a6ed4edbfec37b7806924e8521c83
victor fonseca <victorfonsecass@outlook.com>
2022-02-18 09:39:31 -0300

    user permitted parametters in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    artists_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :fullname, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:username, :password]
+  end
 end
