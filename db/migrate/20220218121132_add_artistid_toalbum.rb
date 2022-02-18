9a2fb2f988eb36f9de0bfd5830725909b15a26dd
victor fonseca <victorfonsecass@outlook.com>
2022-02-18 09:20:23 -0300

    deleted an unecessary migration

--- db/migrate/20220218121132_add_artistid_toalbum.rb
+++
@@ -1,4 +0,0 @@
-class AddArtistidToalbum < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_120856) do
+ActiveRecord::Schema.define(version: 2022_02_18_121132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_02_18_120856) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
     t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
