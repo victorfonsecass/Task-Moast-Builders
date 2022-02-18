ce40068cacdd9cfa5c5175e19cd852fc80eca92d
victor fonseca <victorfonsecass@outlook.com>
2022-02-18 10:44:05 -0300

    controllers with views created

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :albums
+  resources :artists, only: %i[index show new create]
 end
