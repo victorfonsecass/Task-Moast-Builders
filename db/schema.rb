056d1f47f84c29f257da2a79373e1b226b18c406
victor fonseca <victorfonsecass@outlook.com>
2022-02-27 05:15:24 -0300

    em evolução

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_130006) do
+ActiveRecord::Schema.define(version: 2022_02_18_122051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 2022_02_18_130006) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "artist_id"
-    t.index ["artist_id"], name: "index_albums_on_artist_id"
     t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
@@ -41,7 +39,6 @@ ActiveRecord::Schema.define(version: 2022_02_18_130006) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "fullname"
     t.string "username"
-    t.boolean "admin", default: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
